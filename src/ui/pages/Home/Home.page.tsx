9a01f1114ba9174995b887cd221f902de15e7645
osenbal <iqbalmaulana172703@gmail.com>
2023-12-25 11:09:10 +0700

    integration auth

--- src/ui/pages/Home/Home.page.tsx
+++ src/ui/pages/Home/Home.page.tsx
@@ -12,7 +12,7 @@ import { onValue } from 'firebase/database';
 
 const HomePage: React.FC = () => {
   const [toggle, setToggle] = useState(false);
-  const [moisture, setMoisture] = useState<number>(0);
+  const [setMoisture] = useState<number>(0);
   const [suggestion, setSuggestion] = useState<string>('');
 
   useEffect(() => {
